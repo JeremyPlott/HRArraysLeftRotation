7d44984687b75dd2befb0fbec6d0043ec6ed9beb
jeremy_plott <plott.j@pg.com>
2020-01-07 09:09:13 -0500

    Add project files.

---
+++ HRArraysLeftRotation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HRArraysLeftRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3, 4, 5 };
+            int d = 4;
+
+            var rotatedArray = new int[a.Length];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                var offset = (i + d) % a.Length;
+                rotatedArray[i] = a[offset];
+            }
+
+            return rotatedArray;
+
+        }
+    }
+}
